8af17a21c94fd793f6e8ed375cbdaefdddc03f3a
Kevin Brown <kevinbrowntech@gmail.com>
2017-08-20 16:32:53 +1000

    Quick update WIP.

---
+++ src/screens/QuickUpdateScreen/HeaderFilterButton/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+import * as colors from 'kitsu/constants/colors';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+    backgroundColor: 'rgba(200, 200, 200, 0.5)',
+  },
+  text: {
+    marginHorizontal: 14,
+    marginVertical: 2,
+    fontFamily: 'OpenSans',
+    fontWeight: '700',
+    fontSize: 13,
+    color: colors.white,
+  },
+});
+
+export default styles;
