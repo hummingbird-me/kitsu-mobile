0a3e4199cdcd387457a15301dafff84e59fd55de
Josh Madewell <josh.madewell@gmail.com>
2017-08-26 13:19:01 -0500

    fixing styles for empty states on user library

--- src/screens/Profiles/UserLibrary/components/UserLibraryScreen/constants.js
+++ src/screens/Profiles/UserLibrary/components/UserLibraryScreen/constants.js
@@ -2,3 +2,4 @@ export const EMPTY_LIST_CONTAINER_HEIGHT = 125;
 export const POSTER_CARD_HEIGHT = 155;
 export const POSTER_CARD_WIDTH = 105;
 export const EMPTY_LIST_HEIGHT = 110;
+export const CARD_BORDER_RADIUS = 3;
