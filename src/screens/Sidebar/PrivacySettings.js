469abf837c994a8ccb58eecdb40f6cd607892ea4
Enes <edencakir@gmail.com>
2017-09-02 10:26:17 +0300

    redundant imports cleaned

--- src/screens/Sidebar/PrivacySettings.js
+++ src/screens/Sidebar/PrivacySettings.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Switch, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Container, Content } from 'native-base';
 import PropTypes from 'prop-types';
-import * as colors from 'kitsu/constants/colors';
 import { Kitsu, setToken } from 'kitsu/config/api';
 import { SidebarTitle, ItemSeparator, SidebarButton } from './common/';
+import { styles } from './styles';
 
 class PrivacySettings extends React.Component {
   static navigationOptions = {
@@ -64,47 +63,39 @@ class PrivacySettings extends React.Component {
     const loading = this.state.loading;
     const { shareToGlobal } = this.state;
     return (
-      <Container style={styles.containerStyle}>
-        <Content scrollEnabled={false}>
-          <View style={{ flex: 1, marginTop: 77 }}>
-            <SidebarTitle title={'Personal Settings'} />
-            <View
-              style={{
-                flexDirection: 'row',
-                backgroundColor: 'white',
-                paddingHorizontal: 12,
-                paddingVertical: 16,
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}
-            >
-              <Text style={{ fontSize: 14 }}>Share posts to Global Feed</Text>
-              <Switch
-                value={shareToGlobal}
-                onValueChange={v => this.setState({ shareToGlobal: v })}
-              />
-            </View>
-            <ItemSeparator />
-            <View style={{ backgroundColor: 'white', paddingHorizontal: 12, paddingVertical: 8 }}>
-              <Text style={{ fontSize: 10, color: 'grey' }}>
-                If disabled, your posts will only be shared to your followers and guests to your profile.
-              </Text>
-            </View>
-            <SidebarButton
-              title={'Save Privacy Settings'}
-              onPress={this.onSavePrivacySettings}
-              loading={loading}
-            />
-          </View>
-        </Content>
-      </Container>
+      <View style={styles.containerStyle}>
+        <SidebarTitle title={'Personal Settings'} />
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            paddingHorizontal: 12,
+            paddingVertical: 16,
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Text style={{ fontSize: 14 }}>Share posts to Global Feed</Text>
+          <Switch
+            value={shareToGlobal}
+            onValueChange={v => this.setState({ shareToGlobal: v })}
+          />
+        </View>
+        <ItemSeparator />
+        <View style={{ backgroundColor: 'white', paddingHorizontal: 12, paddingVertical: 8 }}>
+          <Text style={{ fontSize: 10, color: 'grey' }}>
+            If disabled, your posts will only be shared to your followers and guests to your profile.
+          </Text>
+        </View>
+        <SidebarButton
+          title={'Save Privacy Settings'}
+          onPress={this.onSavePrivacySettings}
+          loading={loading}
+        />
+      </View>
     );
   }
 }
-
-const styles = {
-  containerStyle: { backgroundColor: colors.listBackPurple },
-};
 
 const mapStateToProps = ({ auth, user }) => ({
   accessToken: auth.tokens.access_token,
