bd64a3172566e4933b13dca6195793ccaf7d749b
Enes <edencakir@gmail.com>
2017-09-06 17:20:22 +0300

    remove unused imports

--- src/screens/Onboarding/styles.js
+++ src/screens/Onboarding/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import * as colors from 'kitsu/constants/colors';
 
 export default StyleSheet.create({
