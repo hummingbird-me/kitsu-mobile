81b4260adc82520347630db914ad612fe30c49bb
Josh Madewell <josh.madewell@gmail.com>
2017-09-10 20:24:10 -0500

    adding simple header component

---
+++ src/components/SimpleHeader/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { flattenCommon } from 'kitsu/common/styles';
+import * as colors from 'kitsu/constants/colors';
+
+export const styles = StyleSheet.create({
+  headerContainer: {
+    alignItems: 'flex-end',
+    backgroundColor: colors.darkPurple,
+    flexDirection: 'row',
+    height: 60,
+  },
+  headerItemText: {
+    ...flattenCommon('text'),
+    color: colors.lightGrey,
+    fontSize: 16,
+  },
+  headerTitleText: {
+    ...flattenCommon('text', 'textHeavy'),
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  leftContainer: {
+    marginRight: 'auto',
+    paddingLeft: 10,
+    paddingBottom: 8,
+  },
+  titleContainer: {
+    position: 'absolute',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 8,
+  },
+  rightContainer: {
+    marginLeft: 'auto',
+    paddingRight: 10,
+    paddingBottom: 8,
+  },
+});
