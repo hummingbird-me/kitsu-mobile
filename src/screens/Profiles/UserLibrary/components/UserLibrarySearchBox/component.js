19b74c8f63360c22f32a95713c6e0476503c32de
Josh Madewell <josh.madewell@gmail.com>
2017-09-11 23:34:36 -0500

    fixing various bugs for user library search screen

--- src/screens/Profiles/UserLibrary/components/UserLibrarySearchBox/component.js
+++ src/screens/Profiles/UserLibrary/components/UserLibrarySearchBox/component.js
@@ -6,6 +6,7 @@ import { SearchBox } from 'kitsu/components/SearchBox';
 import { styles } from './styles';
 
 const MINIMUM_SEARCH_TERM_LENGTH = 3;
+const USER_LIBRARY_SEARCH_SCREEN_ROUTE = 'UserLibrarySearch';
 
 export class UserLibrarySearchBoxComponent extends React.Component {
   static propTypes = {
@@ -23,17 +24,21 @@ export class UserLibrarySearchBoxComponent extends React.Component {
 
   state = {
     canSearch: false,
+    isNewSearch: false,
   }
 
   componentWillReceiveProps(nextProps) {
+    const { routeName } = this.props.navigation.state;
     const currentSearchTerm = this.props.userLibrarySearch.searchTerm;
     const nextSearchTerm = nextProps.userLibrarySearch.searchTerm;
     const isNewSearchTerm = currentSearchTerm !== nextSearchTerm;
     const canSearch = nextSearchTerm.length >= MINIMUM_SEARCH_TERM_LENGTH;
     const isNewProfile = this.props.profile.id !== nextProps.profile.id;
+    const isSearchScreen = routeName !== USER_LIBRARY_SEARCH_SCREEN_ROUTE;
+    const isNewSearch = isNewSearchTerm || isNewProfile;
 
-    if (canSearch && (isNewSearchTerm || isNewProfile)) {
-      this.setState({ canSearch: true });
+    if (canSearch && (isNewSearch || !isSearchScreen)) {
+      this.setState({ isNewSearch, canSearch: true });
     } else if (nextSearchTerm.length === 0) {
       this.debouncedFetch();
     } else {
@@ -45,13 +50,17 @@ export class UserLibrarySearchBoxComponent extends React.Component {
     const { routeName } = this.props.navigation.state;
     const { profile, userLibrarySearch } = this.props;
 
-    this.props.fetchUserLibrary({
-      searchTerm: userLibrarySearch.searchTerm,
-      userId: profile.id,
-    });
+    if (this.state.isNewSearch) {
+      this.props.fetchUserLibrary({
+        searchTerm: userLibrarySearch.searchTerm,
+        userId: profile.id,
+      });
 
-    if (routeName !== 'UserLibrarySearch') {
-      this.props.navigation.navigate('UserLibrarySearch', {
+      this.setState({ isNewSearch: false });
+    }
+
+    if (routeName !== USER_LIBRARY_SEARCH_SCREEN_ROUTE) {
+      this.props.navigation.navigate(USER_LIBRARY_SEARCH_SCREEN_ROUTE, {
         profile,
       });
     }
@@ -61,14 +70,16 @@ export class UserLibrarySearchBoxComponent extends React.Component {
     const { routeName } = this.props.navigation.state;
     const { profile } = this.props;
 
-    if (routeName === 'UserLibrarySearch') {
+    if (routeName === USER_LIBRARY_SEARCH_SCREEN_ROUTE) {
       this.props.fetchUserLibrary({ userId: profile.id });
       this.props.navigation.goBack();
     }
   }, 500);
 
   searchLibrary = () => {
-    if (this.state.canSearch) {
+    const { routeName } = this.props.navigation.state;
+
+    if (this.state.canSearch || routeName !== USER_LIBRARY_SEARCH_SCREEN_ROUTE) {
       this.debouncedSearch();
     }
   }
