7f020a8cb0c1e0e34b04a054b6f7805a6fb97ad5
Kevin Brown <kevinbrowntech@gmail.com>
2017-10-11 16:04:33 +1100

    Moved reactions component to new component style, fixed linter errors.

---
+++ src/screens/Profiles/MediaPages/pages/Reactions/component.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { View, SectionList } from 'react-native';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchMedia, fetchMediaReactions } from 'kitsu/store/media/actions';
+import {
+  ReactionBox,
+  TabHeader,
+  TabContainer,
+} from 'kitsu/screens/Profiles/components';
+import { styles } from './styles';
+
+class Reactions extends Component {
+  static propTypes = {
+    media: PropTypes.object.isRequired,
+    reactions: PropTypes.object.isRequired,
+  }
+
+  static defaultProps = {
+    media: {},
+    reactions: {},
+  }
+
+  handleWriteReaction = () => {
+  }
+
+  renderItem = ({ item }) => {
+    const { media } = this.props;
+
+    return (
+      <ReactionBox
+        reactedMedia={media.canonicalTitle}
+        reaction={item}
+      />
+    );
+  }
+
+  renderListHeader = () => (
+    <TabHeader
+      padded
+      title="Reactions"
+      actionOnPress={this.handleWriteReaction}
+      actionTitle="Write reaction"
+    />
+  )
+
+  renderItemSeparator = () => (
+    <View style={styles.itemSeparator} />
+  )
+
+  renderReactionRows = () => {
+    if (!this.props.reactions) return null;
+    const { reactions } = this.props;
+
+    return (
+      <SectionList
+        sections={[{ data: reactions }]}
+        renderItem={this.renderItem}
+        ItemSeparatorComponent={this.renderItemSeparator}
+        ListHeaderComponent={this.renderListHeader}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <TabContainer>
+        {this.renderReactionRows()}
+      </TabContainer>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { media, reactions } = state.media;
+  const mediaId = 12;
+  return {
+    media: media[mediaId] || {},
+    reactions: reactions[mediaId] || [],
+  };
+};
+
+export const component = connect(mapStateToProps, {
+  fetchMedia,
+  fetchMediaReactions,
+})(Reactions);
---
+++ src/screens/Profiles/MediaPages/pages/Reactions/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  itemSeparator: {
+    alignItems: 'stretch',
+    height: 10,
+  },
+});
