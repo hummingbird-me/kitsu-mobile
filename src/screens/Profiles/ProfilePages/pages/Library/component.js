9331f4c5c9d50c30d87393d55b635793fc588ac9
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-10-20 16:32:08 +1100

    Connected data to Library component.

--- src/screens/Profiles/ProfilePages/pages/Library/component.js
+++ src/screens/Profiles/ProfilePages/pages/Library/component.js
@@ -1,16 +1,69 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import {
   SceneContainer,
 } from 'kitsu/screens/Profiles/components/SceneContainer';
+import { Kitsu } from 'kitsu/config/api';
 
-class Library extends Component {
-  state = {};
+class Library extends PureComponent {
+  static propTypes = {
+    currentUser: PropTypes.object.isRequired,
+  }
+
+  state = {
+    loading: true,
+    data: null,
+  }
+
+  componentDidMount = async () => {
+    try {
+      const data = await Kitsu.findAll('libraryEntries', {
+        fields: {
+          anime: 'slug,posterImage,canonicalTitle,titles,synopsis,subtype,startDate,status,averageRating,popularityRank,ratingRank,episodeCount',
+          users: 'id',
+        },
+        filter: {
+          user_id: this.props.currentUser.id,
+          kind: 'anime',
+        },
+        include: 'anime,user,mediaReaction',
+        page: {
+          // TODO: Connect pagination with flat list
+          offset: 0,
+          limit: 40,
+        },
+        sort: 'status,-progressed_at',
+      });
+
+      this.setState({
+        data,
+        loading: false,
+      });
+    } catch (err) {
+      console.log('Unhandled error while retrieving library entries: ', err);
+    }
+  }
 
   render() {
+    const { loading, data } = this.state;
+
+    if (loading) {
+      // Return loading state.
+      return null;
+    }
+
     return (
       <SceneContainer />
     );
   }
 }
 
-export const component = Library;
+
+const mapStateToProps = ({ user }) => {
+  const { currentUser } = user;
+  return { currentUser };
+};
+
+export const component = connect(mapStateToProps)(Library);
+
