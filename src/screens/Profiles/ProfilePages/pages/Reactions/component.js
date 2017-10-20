ab5f49b0c9212e77226616d6f882b8093c8e2514
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-10-20 17:27:42 +1100

    Connected data to Reactions page.

--- src/screens/Profiles/ProfilePages/pages/Reactions/component.js
+++ src/screens/Profiles/ProfilePages/pages/Reactions/component.js
@@ -1,16 +1,58 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import {
   SceneContainer,
 } from 'kitsu/screens/Profiles/components/SceneContainer';
+import { Kitsu } from 'kitsu/config/api';
 
-class Reactions extends Component {
-  state = {};
+class Reactions extends PureComponent {
+  static propTypes = {
+    currentUser: PropTypes.object.isRequired,
+  }
+
+  state = {
+    loading: true,
+    data: null,
+  }
+
+  componentDidMount = async () => {
+    try {
+      const data = await Kitsu.findAll('mediaReactions', {
+        filter: {
+          user_id: this.props.currentUser.id,
+        },
+        include: 'anime,user,manga',
+        sort: 'upVotesCount',
+      });
+
+      this.setState({
+        data,
+        loading: false,
+      });
+    } catch (err) {
+      console.log('Unhandled error while retrieving reactions: ', err);
+    }
+  }
 
   render() {
+    const { loading, data } = this.state;
+
+    if (loading) {
+      // Return loading state.
+      return null;
+    }
+
     return (
       <SceneContainer />
     );
   }
 }
 
-export const component = Reactions;
+
+const mapStateToProps = ({ user }) => {
+  const { currentUser } = user;
+  return { currentUser };
+};
+
+export const component = connect(mapStateToProps)(Reactions);
