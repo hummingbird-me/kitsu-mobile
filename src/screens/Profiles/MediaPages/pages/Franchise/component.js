cba31e8faaa12e79b7ba05dfd3023b30e8e1f88e
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-10-25 15:09:35 +1100

    Franchise tab is working.

--- src/screens/Profiles/MediaPages/pages/Franchise/component.js
+++ src/screens/Profiles/MediaPages/pages/Franchise/component.js
@@ -1,59 +1,32 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchMedia } from 'kitsu/store/media/actions';
 import { TabHeader } from 'kitsu/screens/Profiles/components/TabHeader';
 import { TabContainer } from 'kitsu/screens/Profiles/components/TabContainer';
 import { MediaRow } from 'kitsu/screens/Profiles/components/MediaRow';
-import { RowSeparator } from 'kitsu/screens/Profiles/components/RowSeparator';
 
-class Franchise extends Component {
-  static propTypes = {
-    media: PropTypes.object.isRequired,
-  }
+export const component = ({ media }) => {
+  const title = media.titles.en || media.titles.en_jp || media.titles.jp_jp;
 
-  static defaultProps = {
-    media: {},
-  }
-
-  render = () => {
-    const { media } = this.props;
-
-    const data = [{
-      title: media.canonicalTitle,
-      summary: media.synopsis,
-      thumbnail: media.posterImage && media.posterImage.large,
-    }];
-
-    return (
-      <TabContainer light padded>
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <MediaRow
-              title={item.title}
-              summary={item.summary}
-              thumbnail={{ uri: item.thumbnail }}
-              summaryLines={12}
-            />
-          )}
-          ItemSeparatorComponent={() => <RowSeparator />}
-          ListHeaderComponent={() => <TabHeader title="Franchise" contentDark />}
-        />
-      </TabContainer>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  const { media } = state.media;
-  const mediaId = 12;
-  return {
-    media: media[mediaId] || {},
-  };
+  return (
+    <TabContainer light padded>
+      <TabHeader title="Franchise" contentDark />
+      <MediaRow
+        title={title}
+        summary={media.synopsis}
+        thumbnail={{ uri: media.posterImage.large }}
+        summaryLines={12}
+      />
+    </TabContainer>
+  );
 };
 
-export const component = connect(mapStateToProps, {
-  fetchMedia,
-})(Franchise);
+component.propTypes = {
+  media: PropTypes.shape({
+    titles: PropTypes.object.isRequired,
+    synopsis: PropTypes.string.isRequired,
+    posterImage: PropTypes.shape({
+      large: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
