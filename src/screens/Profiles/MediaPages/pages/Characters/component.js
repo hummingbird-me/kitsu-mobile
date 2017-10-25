2563649d2e4d6a8f39e476e8f7e5dec9fb14cdf9
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-10-25 14:45:23 +1100

    Removing redux wrapper.

--- src/screens/Profiles/MediaPages/pages/Characters/component.js
+++ src/screens/Profiles/MediaPages/pages/Characters/component.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchMedia, fetchMediaCastings } from 'kitsu/store/media/actions';
 import { TabHeader } from 'kitsu/screens/Profiles/components/TabHeader';
 import { TabContainer } from 'kitsu/screens/Profiles/components/TabContainer';
 import { PhotoGrid } from 'kitsu/screens/Profiles/components/PhotoGrid';
@@ -34,16 +32,4 @@ class Characters extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  const { media, castings } = state.media;
-  const mediaId = 12;
-  return {
-    media: media[mediaId] || {},
-    castings: castings[mediaId] || [],
-  };
-};
-
-export const component = connect(mapStateToProps, {
-  fetchMedia,
-  fetchMediaCastings,
-})(Characters);
+export const component = Characters;
