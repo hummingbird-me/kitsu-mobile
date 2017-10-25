88e7c037c8a7669cb66db3cb33ec97c33d0c998d
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-10-25 15:18:30 +1100

    Removing unnecessary imports now that I've refactored the component.

--- src/screens/Profiles/MediaPages/pages/Franchise/component.js
+++ src/screens/Profiles/MediaPages/pages/Franchise/component.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import React from 'react';
 import PropTypes from 'prop-types';
+
 import { TabHeader } from 'kitsu/screens/Profiles/components/TabHeader';
 import { TabContainer } from 'kitsu/screens/Profiles/components/TabContainer';
 import { MediaRow } from 'kitsu/screens/Profiles/components/MediaRow';
