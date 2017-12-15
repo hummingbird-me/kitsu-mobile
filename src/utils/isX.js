0dc46eb436a44d4f60f6f4ea5ddd78e5e4a7d1aa
Enes <edencakir@gmail.com>
2017-12-16 00:55:54 +0300

    add X compatibility :rocket:

---
+++ src/utils/isX.js
@@ -0,0 +1,25 @@
+import { DeviceInfo, Dimensions, NativeModules, Platform } from 'react-native';
+
+// See https://mydevice.io/devices/ for device dimensions
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+
+const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window');
+
+const { PlatformConstants = {} } = NativeModules;
+const { minor = 0 } = PlatformConstants.reactNativeVersion || {};
+
+export const paddingX = 20;
+export const isX = (() => {
+  if (Platform.OS === 'web') return false;
+
+  if (minor >= 50) {
+    return DeviceInfo.isIPhoneX_deprecated;
+  }
+
+  return (
+    Platform.OS === 'ios' &&
+    ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
+      (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT))
+  );
+})();
