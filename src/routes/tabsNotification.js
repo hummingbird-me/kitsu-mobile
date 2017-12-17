9f0b60de869bee3f703ec7c26e422f257df1942f
Enes <edencakir@gmail.com>
2017-12-17 19:28:47 +0300

    hacky navigation when app is launched from a notification

---
+++ src/routes/tabsNotification.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { fetchCurrentUser } from 'kitsu/store/user/actions';
+import { fetchAlgoliaKeys } from 'kitsu/store/app/actions';
+import { tabRed, listBackPurple } from 'kitsu/constants/colors';
+import SearchStack from './search';
+import NotificationsStack from './notification';
+import QuickUpdateStack from './quickUpdate';
+import FeedStack from './feed';
+import SidebarStack from './sidebar';
+
+const Tabs = TabNavigator(
+  {
+    Feed: {
+      screen: FeedStack,
+    },
+    Search: {
+      screen: SearchStack,
+    },
+    QuickUpdate: {
+      screen: QuickUpdateStack,
+    },
+    Notifications: {
+      screen: NotificationsStack,
+    },
+    Menu: {
+      screen: SidebarStack,
+    },
+  },
+  {
+    lazy: true,
+    tabBarPosition: 'bottom',
+    swipeEnabled: Platform.OS === 'ios',
+    initialRouteName: 'Notifications',
+    tabBarOptions: {
+      activeTintColor: tabRed,
+      inactiveBackgroundColor: listBackPurple,
+      activeBackgroundColor: listBackPurple,
+      showLabel: false,
+      showIcon: true,
+      style: {
+        height: 44.96,
+        borderTopWidth: 0,
+        backgroundColor: listBackPurple,
+      },
+      indicatorStyle: {
+        backgroundColor: tabRed,
+      },
+      backgroundColor: listBackPurple,
+    },
+  },
+);
+
+class TabsNav extends React.PureComponent {
+  static propTypes = {
+    fetchCurrentUser: PropTypes.func.isRequired,
+  };
+
+  componentWillMount() {
+    this.props.fetchCurrentUser();
+    this.props.fetchAlgoliaKeys();
+  }
+
+  render() {
+    return <Tabs screenProps={{ rootNavigation: this.props.navigation }} />;
+  }
+}
+
+const mapper = () => ({});
+
+export default connect(mapper, { fetchCurrentUser, fetchAlgoliaKeys })(TabsNav);
