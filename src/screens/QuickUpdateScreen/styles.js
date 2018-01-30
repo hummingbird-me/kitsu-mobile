099edd9eac4d6226abc95aa5205c6b2f00f2e62f
Enes <edencakir@users.noreply.github.com>
2018-01-30 23:28:36 +0300

    More QU (#141)
    * card design change
    
    * card design ui improvements
    
    * card positioned & resized backgroudn image
    
    * request template
    
    * ui improvements, lists
    
    * editor template
    
    * loader background color added
    
    * episode number, title and progress bar style
    
    * minor stuff
    
    * export mediatag component
    
    * update menu button styles
    
    * mark as watched
    
    * rating modals template
    
    * remove duplicate use of rating components & export
    
    * style work of simple rating
    
    * revert to modal rating
    
    * fix iphone x ui
    
    * Comment reply banner (#136)
    
    * Remove nested input box
    
    * Show reply banner on comment input
    
    * Handle submission of replies from PostDetails
    
    * Password Reset (#135)
    
    * Hookup password reset email screen
    
    * Remove unused imports
    
    * Implement the active filter request
    
    * Render manga cards
    
    * Update QuickUpdateCard to handle when progress is 0
    
    * Update feed loading
    
    - Fix feed pagination
    - Don't request or render feed for new items
    
    * Update refetching library entries
    
    - Match code from initial request
    - Refetch discussions
    
    * Update pagination on feeds
    
    * Bug fixes (#134)
    
    * Fixed display of time watching anime in onboarding.
    Fixed typo in Parallel Universe category.
    
    * Temporarily disabled codepush for android.
    
    * Added alternate text for non-aozora user onboarding.
    Fixed not being able to select favourite categories in onboarding.
    
    * Disabled swipe gestures on tabs.
    
    * Added Global Feed.
    
    * Fixed media page navigation issues in sidebar.
    Added back button to profile view while it's loading.
    
    * Added navigation to post upon tap of likes/comments.
    Added loading indicator in create post.
    Added error displaying in create post.
    
    * Fixed notification badge clipping on android.
    
    * More navigation fixes on sidebar.
    
    * Updated all navigation bars to be the same height across different platforms.
    Fixed some styling issues on android.
    
    * Minor fix.
    
    * Fixed same navigation display issues for iPhone X.
    
    * More iPhone X Fixes.
    
    * more iPhoneX fixes.
    Added a constant for navigation bar height.
    
    * Woops forgot something.
    
    * Code cleanup.
    
    * pull master
    
    * null data handling
    
    * editor media tag
    
    * undo empty list mistake
    
    * discussion post api hook
    
    * undo cursor change
    
    * fix header style problems
    
    * remove unused variables & console logs
    
    * conditional rendering & required data handling
    
    * uris wishes
    
    * minor fixes
    
    * fix iphone x editor button position
    
    * fix crash & title overflow
    
    * empty views
    
    * fix media tag title and disable press
    
    * make it scroll vertically with sticky header
    
    * hide header as well
    
    * go up on double tab press
    
    * remove logs

--- src/screens/QuickUpdateScreen/styles.js
+++ src/screens/QuickUpdateScreen/styles.js
@@ -89,6 +89,37 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
+  unstartedWrapper: {},
+  statusWrapper: {
+    marginTop: 4,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.darkPurple,
+  },
+  statusTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: colors.white,
+    fontFamily: 'OpenSans',
+    textAlign: 'center',
+    margin: 4,
+    width: 240,
+  },
+  statusText: {
+    width: 240,
+    fontSize: 12,
+    color: colors.lightGrey,
+    fontFamily: 'OpenSans',
+    textAlign: 'center',
+    margin: 4,
+  },
+  statusImage: {
+    marginTop: 16,
+    width: 140,
+    height: 160,
+    resizeMode: 'contain',
+  },
 });
 
 export default styles;
