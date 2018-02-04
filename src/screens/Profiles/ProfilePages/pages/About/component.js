3d9d2d819f389a4e9aa3e309128bba612db0460d
Thomas McNiven <hello@vevix.net>
2018-02-04 21:56:51 +1000

    Fix waifu display on Profile about

--- src/screens/Profiles/ProfilePages/pages/About/component.js
+++ src/screens/Profiles/ProfilePages/pages/About/component.js
@@ -8,6 +8,7 @@ import { SceneLoader } from 'kitsu/components/SceneLoader';
 import { InfoRow } from 'kitsu/screens/Profiles/components/InfoRow';
 import { TabContainer } from 'kitsu/screens/Profiles/components/TabContainer';
 import { MediaRow } from 'kitsu/screens/Profiles/components/MediaRow';
+import capitalize from 'lodash/capitalize';
 
 const ItemSeparator = () => <View style={{ height: 10 }} />;
 
@@ -25,7 +26,7 @@ class About extends Component {
     const { userId } = this.props;
     try {
       const data = await Kitsu.one('users', userId).get({
-        include: 'profileLinks.profileLinkSite,favorites.item',
+        include: 'waifu',
       });
 
       this.setState({
@@ -44,10 +45,10 @@ class About extends Component {
     if (loading) return <SceneLoader />;
 
     const aboutRows = [
-      { key: 'gender', label: 'Gender', content: data.gender },
+      { key: 'gender', label: 'Gender', content: capitalize(data.gender) },
       { key: 'location', label: 'Location', content: data.location },
       { key: 'birthday', label: 'Birthday', content: moment(data.birthday).format('MMMM Do') },
-      { key: 'joinDate', label: 'Join date', content: moment(data.createdAt).format('MMMM Do, YYYY') },
+      { key: 'joinDate', label: 'Joined', content: moment(data.createdAt).format('MMMM Do, YYYY') },
     ];
 
     if (data.waifu) {
