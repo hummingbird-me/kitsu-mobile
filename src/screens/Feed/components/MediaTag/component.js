1f9ab35181c625da4344b0f61823c1c9f1dcf280
Mikunj varsani <mikunj@live.com.au>
2018-02-13 10:32:37 +1100

    Fixed media tags showing Episode instead of Chapter number for manga. Fixed post layout when only media tag and embed are present.

--- src/screens/Feed/components/MediaTag/component.js
+++ src/screens/Feed/components/MediaTag/component.js
@@ -1,36 +1,39 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, ViewPropTypes } from 'react-native';
 import { StyledText } from 'kitsu/components/StyledText';
 import PropTypes from 'prop-types';
 import { styles } from './styles';
 
-export const MediaTag = ({ disabled, media, episode, navigation }) => (
-  <View style={styles.mediaTagView}>
-    <TouchableOpacity
-      disabled={disabled}
-      onPress={() =>
-        navigation.navigate('MediaPages', { mediaId: media.id, mediaType: media.type })}
-      style={styles.mediaTag}
-    >
-      <StyledText color="green" size="xxsmall">
-        {media.canonicalTitle}
-      </StyledText>
-    </TouchableOpacity>
-    {episode && (
+export const MediaTag = ({ disabled, media, episode, navigation, style }) => {
+  const episodePrefix = media.type === 'anime' ? 'E' : 'CH';
+  return (
+    <View style={[styles.mediaTagView, style]}>
       <TouchableOpacity
         disabled={disabled}
         onPress={() =>
           navigation.navigate('MediaPages', { mediaId: media.id, mediaType: media.type })}
-        style={styles.episodeTagView}
+        style={styles.mediaTag}
       >
-        <View style={styles.episodeTagLine} />
-        <View style={styles.mediaTag}>
-          <StyledText color="green" size="xxsmall">{`E ${episode.number}`}</StyledText>
-        </View>
+        <StyledText color="green" size="xxsmall">
+          {media.canonicalTitle}
+        </StyledText>
       </TouchableOpacity>
-    )}
-  </View>
-);
+      {episode && (
+        <TouchableOpacity
+          disabled={disabled}
+          onPress={() =>
+            navigation.navigate('MediaPages', { mediaId: media.id, mediaType: media.type })}
+          style={styles.episodeTagView}
+        >
+          <View style={styles.episodeTagLine} />
+          <View style={styles.mediaTag}>
+            <StyledText color="green" size="xxsmall">{`${episodePrefix} ${episode.number}`}</StyledText>
+          </View>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
 
 MediaTag.propTypes = {
   media: PropTypes.shape({
@@ -41,9 +44,11 @@ MediaTag.propTypes = {
   }),
   navigation: PropTypes.object.isRequired,
   disabled: PropTypes.bool,
+  style: ViewPropTypes.style,
 };
 
 MediaTag.defaultProps = {
   episode: null,
   disabled: false,
+  style: null,
 };
