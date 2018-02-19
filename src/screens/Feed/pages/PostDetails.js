1f9791398246202ffb9967c1c1279a91351ef4c2
Mikunj varsani <mikunj@live.com.au>
2018-02-19 22:39:01 +1100

    Added better gif handling on comments.

--- src/screens/Feed/pages/PostDetails.js
+++ src/screens/Feed/pages/PostDetails.js
@@ -66,8 +66,12 @@ export default class PostDetails extends PureComponent {
   onCommentChanged = comment => this.setState({ comment });
 
   onGifSelected = (gif) => {
-    this.setState({ comment: gif.images.original.url }, () => {
-      this.onSubmitComment();
+    const gifUrl = gif.images.original.url;
+    const comment = this.state.comment.trim();
+    const newComment = isEmpty(comment) ? gifUrl : `${comment}\n${gifUrl}`;
+    this.setState({ comment: newComment }, () => {
+      // Submit gif if comment was empty
+      if (isEmpty(comment)) this.onSubmitComment();
     });
   }
 
