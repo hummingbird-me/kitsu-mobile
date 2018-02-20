6f804e75a72a70b204e999d7ac3b5e09656aa9a3
Mikunj varsani <mikunj@live.com.au>
2018-02-20 15:53:30 +1100

    Only enable sentry on production builds. Fix android build issue

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,12 @@
 import { AppRegistry } from 'react-native';
+import { Sentry } from 'react-native-sentry';
 import App from './src/App';
 
-import { Sentry } from 'react-native-sentry';
+Sentry.config('https://068b9ab849bf4485beb4884adcc5be83:8c57373b9bb4410f99ebfd17878c739a@sentry.io/200469');
 
-Sentry.config('https://068b9ab849bf4485beb4884adcc5be83:8c57373b9bb4410f99ebfd17878c739a@sentry.io/200469').install();
-
+// Only enable sentry on production builds
+if (!__DEV__) {
+  Sentry.install();
+}
 
 AppRegistry.registerComponent('kitsu_mobile', () => App);
