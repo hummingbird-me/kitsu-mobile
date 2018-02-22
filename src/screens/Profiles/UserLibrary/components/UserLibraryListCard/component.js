e13f5627c26bd7dd0f2e0d582d10478085dfacd1
Mikunj varsani <mikunj@live.com.au>
2018-02-22 12:41:12 +1100

    Fixed up text in library list card

--- src/screens/Profiles/UserLibrary/components/UserLibraryListCard/component.js
+++ src/screens/Profiles/UserLibrary/components/UserLibraryListCard/component.js
@@ -158,9 +158,6 @@ export class UserLibraryListCard extends React.Component {
     const canEdit = this.props.profile.id === this.props.currentUser.id;
     const maxProgress = this.getMaxProgress();
 
-    const textMapping = HEADER_TEXT_MAPPING.includes(libraryEntry.status) ?
-      HEADER_TEXT_MAPPING[libraryEntry.status][libraryType] : '-';
-
     return (
       <Swipeable
         onRightActionActivate={this.onRightActionActivate}
@@ -185,11 +182,17 @@ export class UserLibraryListCard extends React.Component {
           { libraryEntry.status !== this.props.libraryStatus &&
             <View style={styles.moved}>
               <View style={styles.horizontalRule} />
-              <Text style={styles.movedText}>
-                Moved to <Text style={styles.movedToText}>
-                  {textMapping}
+              { HEADER_TEXT_MAPPING.includes(libraryEntry.status) ?
+                <Text style={styles.movedText}>
+                  Moved to <Text style={styles.movedToText}>
+                    {HEADER_TEXT_MAPPING[libraryEntry.status][libraryType]}
+                  </Text>
                 </Text>
-              </Text>
+                :
+                <Text style={styles.movedText}>
+                  Removed from Library
+                </Text>
+              }
               <View style={styles.horizontalRule} />
             </View>
           }
