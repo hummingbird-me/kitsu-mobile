a3f817df77fb9bec666a5b62e747ba21448cfb8d
Thomas McNiven <hello@vevix.net>
2018-02-28 09:08:58 +1000

    Initial work on adding RecycleListView to feeds

--- src/screens/Feed/components/CreatePostRow/styles.js
+++ src/screens/Feed/components/CreatePostRow/styles.js
@@ -1,9 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { scenePadding } from 'kitsu/screens/Feed/constants';
 
 export const styles = StyleSheet.create({
   wrap: {
     backgroundColor: '#FFFFFF',
     padding: scenePadding,
+    minWidth: Dimensions.get('window').width,
   },
 });
--- src/screens/Feed/components/Post/styles.js
+++ src/screens/Feed/components/Post/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import * as colors from 'kitsu/constants/colors';
 import { scenePadding } from 'kitsu/screens/Feed/constants';
 
@@ -6,6 +6,9 @@ export const styles = StyleSheet.create({
   wrap: {
     backgroundColor: '#FFFFFF',
     marginTop: 10,
+    // Force max width as `forceNonDeterministicRendering` on
+    // `RecycleListView` will also affect width.
+    minWidth: Dimensions.get('window').width,
   },
   postFooter: {
     borderTopWidth: StyleSheet.hairlineWidth,
