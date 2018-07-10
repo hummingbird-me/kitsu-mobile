7a98ca42ea6a6e9ebe896a973fb48288bf1fa625
Mikunj varsani <mikunj@live.com.au>
2018-07-10 13:18:13 +1000

    Removed unused files.

--- src/screens/Profiles/index.js
+++ src/screens/Profiles/index.js
@@ -1,19 +1,5 @@
-import FavoriteCharacters from './FavoriteCharacters';
-import FavoriteMedia from './FavoriteMedia';
-import MediaScreen from './MediaScreen';
-import NetworkScreen from './NetworkScreen';
-import ProfileScreen from './ProfileScreen';
-
 export {
   User,
   UserLibraryEditScreen,
   UserLibraryListScreen,
 } from './UserLibrary';
-
-export {
-  FavoriteCharacters,
-  FavoriteMedia,
-  MediaScreen,
-  NetworkScreen,
-  ProfileScreen,
-};
