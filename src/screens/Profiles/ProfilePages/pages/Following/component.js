e74dd91c2f80bdac16d9bc2b7de3ea75a5906d32
Hiep Can <hiepcan@uw.edu>
2018-07-21 19:34:12 -0700

    Add infinite scrolling for Following page

--- src/screens/Profiles/ProfilePages/pages/Following/component.js
+++ src/screens/Profiles/ProfilePages/pages/Following/component.js
@@ -1,29 +1,56 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import FastImage from 'react-native-fast-image';
+import URL from 'url-parse';
 import { View, FlatList, Text } from 'react-native';
 import { SceneLoader } from 'kitsu/components/SceneLoader';
 import { TabContainer } from 'kitsu/screens/Profiles/components/TabContainer';
 import { ProgressiveImage } from 'kitsu/components/ProgressiveImage';
 import { getImgixCoverImage } from 'kitsu/utils/imgix';
-import { RowSeparator } from 'kitsu/screens/Profiles/components/RowSeparator';
 import { defaultCover, defaultAvatar } from 'kitsu/constants/app';
+import { offWhite } from 'kitsu/constants/colors';
+import { isEmpty } from 'lodash';
 import { Kitsu } from 'kitsu/config/api';
 import { styles } from './styles';
 
 class Following extends PureComponent {
   static propTypes = {
-    userId: PropTypes.number.isRequired,
+    userId: PropTypes.string.isRequired,
   }
 
   state = {
     loading: true,
-    data: null,
+    isLoadingNextPage: false,
+    following: [],
   }
 
-  componentDidMount = async () => {
+  componentDidMount = () => {
+    this.fetchPage();
+  }
+
+  fetchNextPage = async () => {
+    this.setState({ isLoadingNextPage: true });
+    await this.fetchPage();
+    this.setState({ isLoadingNextPage: false });
+  }
+
+  offset = undefined;
+  canFetchNext = true;
+
+  fetchPage = async () => {
+    const PAGE_SIZE = 20;
+
+    if (this.isFetchingPage) return;
+    this.isFetchingPage = true;
+
+    if (!this.canFetchNext) {
+      this.offset = undefined;
+      this.isFetchingPage = false;
+      return;
+    }
+
     try {
-      const following = await Kitsu.findAll('follows', {
+      const result = await Kitsu.findAll('follows', {
         fields: {
           users: 'avatar,coverImage,name,slug',
         },
@@ -32,14 +59,30 @@ class Following extends PureComponent {
         },
         include: 'followed',
         sort: '-created_at',
+        page: {
+          offset: this.offset,
+          limit: PAGE_SIZE,
+        },
       });
+
+      this.canFetchNext = !isEmpty(result && result.links && result.links.next);
+      const nextPage = new URL(result.links.next, true);
+      this.offset = nextPage.query['page[offset]'];
+      const following = [...this.state.following, ...result];
 
       this.setState({
         following,
         loading: false,
       });
     } catch (err) {
-      console.log('Unhandled error while retrieving groups: ', err);
+      console.log('Unhandled error while retrieving following: ', err);
+
+      this.setState({
+        following: [],
+        error,
+      });
+    } finally {
+      this.isFetchingPage = false;
     }
   }
 
@@ -77,10 +120,22 @@ class Following extends PureComponent {
     return (
       <TabContainer>
         <FlatList
-          listKey="groups"
+          keyExtractor={(item, index) => index.toString()}
           data={following}
           renderItem={this.renderGroupItem}
-          ItemSeparatorComponent={() => <RowSeparator />}
+          onMomentumScrollBegin={() => {
+            this.onEndReachedCalledDuringMomentum = false;
+          }}
+          onEndReached={() => {
+            if (!this.onEndReachedCalledDuringMomentum) {
+              this.fetchNextPage();
+              this.onEndReachedCalledDuringMomentum = false;
+            }
+          }}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={() => this.state.isLoadingNextPage && (
+            <SceneLoader color={offWhite} />
+          )}
         />
       </TabContainer>
     );
