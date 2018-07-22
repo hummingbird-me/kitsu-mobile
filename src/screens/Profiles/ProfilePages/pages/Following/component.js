ad48db1ecaf24b5048404ce0234c0daede39c6db
Hiep Can <hiepcan@uw.edu>
2018-07-21 23:29:50 -0700

    Add refreshing to scrolling and link to profile page for following

--- src/screens/Profiles/ProfilePages/pages/Following/component.js
+++ src/screens/Profiles/ProfilePages/pages/Following/component.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import FastImage from 'react-native-fast-image';
 import URL from 'url-parse';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, Text, TouchableOpacity } from 'react-native';
 import { SceneLoader } from 'kitsu/components/SceneLoader';
 import { TabContainer } from 'kitsu/screens/Profiles/components/TabContainer';
 import { ProgressiveImage } from 'kitsu/components/ProgressiveImage';
@@ -16,16 +16,24 @@ import { styles } from './styles';
 class Following extends PureComponent {
   static propTypes = {
     userId: PropTypes.string.isRequired,
+    navigation: PropTypes.object.isRequired,
   }
 
   state = {
     loading: true,
     isLoadingNextPage: false,
+    refreshing: false,
     following: [],
   }
 
   componentDidMount = () => {
     this.fetchPage();
+  }
+
+  onRefresh = async () => {
+    this.setState({ refreshing: true });
+    await this.fetchPage({ reset: true });
+    this.setState({ refreshing: false });
   }
 
   fetchNextPage = async () => {
@@ -37,11 +45,16 @@ class Following extends PureComponent {
   offset = undefined;
   canFetchNext = true;
 
-  fetchPage = async () => {
+  fetchPage = async ({ reset = false } = {}) => {
     const PAGE_SIZE = 20;
 
     if (this.isFetchingPage) return;
     this.isFetchingPage = true;
+
+    if (reset) {
+      this.offset = undefined;
+      this.canFetchNext = true;
+    }
 
     if (!this.canFetchNext) {
       this.offset = undefined;
@@ -64,11 +77,12 @@ class Following extends PureComponent {
           limit: PAGE_SIZE,
         },
       });
+      console.log(result);
 
       this.canFetchNext = !isEmpty(result && result.links && result.links.next);
       const nextPage = new URL(result.links.next, true);
       this.offset = nextPage.query['page[offset]'];
-      const following = [...this.state.following, ...result];
+      const following = reset ? [...result] : [...this.state.following, ...result];
 
       this.setState({
         following,
@@ -80,15 +94,18 @@ class Following extends PureComponent {
       this.setState({
         following: [],
         error,
+        refreshing: false,
       });
     } finally {
       this.isFetchingPage = false;
     }
   }
 
-  renderGroupItem = ({ item }) => {
+  keyExtractor = item => `${item.id}-${item.updatedAt}`;
+
+  renderFollowingItem = ({ item }) => {
     if (!item || !item.followed) return null;
-    const { avatar, coverImage, name } = item.followed;
+    const { avatar, coverImage, name, id } = item.followed;
     return (
       <View style={styles.userContainer}>
         <ProgressiveImage
@@ -97,11 +114,16 @@ class Following extends PureComponent {
           source={{ uri: (coverImage && getImgixCoverImage(coverImage)) || defaultCover }}
         />
         <View style={styles.userProfileContainer}>
-          <FastImage
-            style={styles.userProfileImage}
-            source={{ uri: (avatar && avatar.medium) || defaultAvatar }}
-            cache="web"
-          />
+          <TouchableOpacity onPress={() => {
+            this.props.navigation.navigate('ProfilePages', { userId: id });
+          }}
+          >
+            <FastImage
+              style={styles.userProfileImage}
+              source={{ uri: (avatar && avatar.medium) || defaultAvatar }}
+              cache="web"
+            />
+          </TouchableOpacity>
           <View style={styles.userProfileTextWrapper}>
             <Text style={styles.userProfileName}>{name}</Text>
           </View>
@@ -111,7 +133,7 @@ class Following extends PureComponent {
   }
 
   render() {
-    const { following, loading } = this.state;
+    const { following, loading, isLoadingNextPage } = this.state;
 
     if (loading) {
       return <SceneLoader />;
@@ -120,22 +142,26 @@ class Following extends PureComponent {
     return (
       <TabContainer>
         <FlatList
-          keyExtractor={(item, index) => index.toString()}
+          keyExtractor={this.keyExtractor}
           data={following}
-          renderItem={this.renderGroupItem}
+          refreshing={this.state.refreshing}
+          onRefresh={this.onRefresh}
+          renderItem={this.renderFollowingItem}
           onMomentumScrollBegin={() => {
             this.onEndReachedCalledDuringMomentum = false;
           }}
           onEndReached={() => {
             if (!this.onEndReachedCalledDuringMomentum) {
               this.fetchNextPage();
-              this.onEndReachedCalledDuringMomentum = false;
+              this.onEndReachedCalledDuringMomentum = true;
             }
           }}
-          onEndReachedThreshold={0.5}
-          ListFooterComponent={() => this.state.isLoadingNextPage && (
+          onEndReachedThreshold={0.6}
+          ListFooterComponent={() => isLoadingNextPage && (
             <SceneLoader color={offWhite} />
           )}
+
+          removeClippedSubviews
         />
       </TabContainer>
     );
