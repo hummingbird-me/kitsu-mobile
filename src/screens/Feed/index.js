075554a2126beb8b663b61c783a511a9a1e1a7da
Mikunj Varsani <mikunj@live.com.au>
2018-10-15 17:59:36 +1100

    Undo launchMode change. Register deep links after interactions have run.

--- src/screens/Feed/index.js
+++ src/screens/Feed/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, View, StyleSheet, Platform } from 'react-native';
+import { StatusBar, View, StyleSheet, Platform, InteractionManager } from 'react-native';
 import { connect } from 'react-redux';
 import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view';
 import PropTypes from 'prop-types';
@@ -57,7 +57,9 @@ class Feed extends React.PureComponent {
   };
 
   componentDidMount() {
-    registerDeepLinks();
+    InteractionManager.runAfterInteractions(() => {
+      registerDeepLinks();
+    });
     this.fetchFeed();
   }
 
