8c09906897351025a02a00bc1564c4a3f81a38ff
Thomas McNiven <hello@vevix.net>
2018-10-17 18:21:29 +1000

    Add "Load More" button to feeds
    - Feeds on the Media and Profile pages

--- src/screens/Profiles/ProfilePages/pages/Feed/component.js
+++ src/screens/Profiles/ProfilePages/pages/Feed/component.js
@@ -10,7 +10,10 @@ import { CreatePostRow } from 'kitsu/screens/Feed/components/CreatePostRow';
 import { View, FlatList } from 'react-native';
 import { Screens, NavigationActions } from 'kitsu/navigation';
 import { Navigation } from 'react-native-navigation';
-
+import URL from 'url-parse';
+import { listBackPurple } from 'kitsu/constants/colors';
+import { Button } from 'kitsu/components/Button';
+import { isEmpty } from 'lodash';
 
 class FeedComponent extends PureComponent {
   static propTypes = {
@@ -26,12 +29,13 @@ class FeedComponent extends PureComponent {
 
   state = {
     error: null,
-    loading: false,
     feed: [],
+    loadingFeed: true,
+    loadingNextFeed: false,
   }
 
   componentDidMount() {
-    this.fetchFeed();
+    this.fetchFeed({ reset: true });
   }
 
   navigateToPost = (props) => {
@@ -43,33 +47,50 @@ class FeedComponent extends PureComponent {
     });
   }
 
-  fetchFeed = async () => {
+  canFetchNextFeed = true;
+  feedCursor = undefined;
+  fetchFeed = async ({ reset = false } = {}) => {
     const { userId } = this.props;
 
-    this.setState({ loading: true });
+    if (reset) {
+      this.canFetchNextFeed = true;
+      this.feedCursor = undefined;
+      this.setState({ loadingFeed: true });
+    } else if (this.canFetchNextFeed) {
+      this.setState({ loadingNextFeed: true });
+    }
 
+    let data = [];
     try {
       const result = await Kitsu.one('userFeed', userId).get({
         include: 'media,actor,unit,subject,target,target.user,target.target_user,target.spoiled_unit,target.media,target.target_group,subject.user,subject.target_user,subject.spoiled_unit,subject.media,subject.target_group,subject.followed,subject.library_entry,subject.anime,subject.manga,subject.uploads,target.uploads',
         filter: {
           kind: 'posts',
         },
-        // TODO: Maybe later find a way for infinite scrolling
         page: {
-          limit: 40,
+          cursor: this.feedCursor,
+          limit: 10,
         },
       });
 
+      // I need to read the cursor value out of the 'next' link in the result.
+      this.canFetchNextFeed = !isEmpty(result && result.links && result.links.next);
+      const url = new URL(result.links.next, true);
+      this.feedCursor = url.query['page[cursor]'];
+
       // Need to change this here if we want to also display media updates, follows, etc
       const feed = preprocessFeed(result).filter(i => i.type === 'posts');
-
-      this.setState({
-        feed,
-        loading: false,
-      });
+      data = reset ? [...feed] : [...this.state.feed, ...feed];
     } catch (error) {
       console.log(error);
-      this.setState({ error, loading: false });
+      data = [];
+      this.setState({ error });
+    } finally {
+      this.setState({
+        feed: data,
+        loadingFeed: false,
+        loadingNextFeed: false,
+      });
     }
   }
 
@@ -93,7 +114,7 @@ class FeedComponent extends PureComponent {
 
   render() {
     const { profile } = this.props;
-    const { loading, feed } = this.state;
+    const { loadingFeed, loadingNextFeed, feed } = this.state;
 
     // TODO: Show error state here
     return (
@@ -102,15 +123,26 @@ class FeedComponent extends PureComponent {
           <CreatePostRow onPress={this.navigateToCreatePost} targetUser={profile} />
         </View>
         {/* Feed */}
-        { loading ?
+        { loadingFeed ?
           <SceneLoader />
           :
-          <FlatList
-            listKey="feed"
-            data={feed || []}
-            keyExtractor={item => `${item.id}`}
-            renderItem={this.renderItem}
-          />
+          <React.Fragment>
+            <FlatList
+              listKey="feed"
+              data={feed || []}
+              keyExtractor={item => `${item.id}`}
+              renderItem={this.renderItem}
+              />
+            {/* @Temporary - Load more button */}
+            { this.canFetchNextFeed && (
+              <Button
+                style={{ backgroundColor: listBackPurple }}
+                title="Load More"
+                onPress={() => { this.fetchFeed(); } }
+                loading={loadingNextFeed}
+              />
+            )}
+          </React.Fragment>
         }
       </SceneContainer>
     );
