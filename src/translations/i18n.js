94094ef2c5d2fdf3cef95c7e0f3ea57fb484df0f
Richard <RAVMN@users.noreply.github.com>
2020-04-12 18:19:02 -0400

    Update i18n.js
    uses React Native Languages to get system language

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -1,18 +1,15 @@
 import I18n from "i18n-js";
 import * as RNLanguages from "react-native-languages";
 
-import en from "kitsu/translations/en.json";
-import es from "kitsu/translations/es.json";
+import en from "./en";
+import es from "./es";
 
-const locales = RNLanguages.getLocales();
+const locales = RNLanguages.language;
 
-if (Array.isArray(locales)) {
-  I18n.locale = locales[0].languageTag;
-}
+I18n.locale = locales;
 
 I18n.fallbacks = true;
-
-I18n.translation = {
+I18n.translations = {
   en,
   es
 };
