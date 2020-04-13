a9a3095132f2f66b55879e1d5309133cf4275793
Richard <RAVMN@users.noreply.github.com>
2020-04-12 20:56:47 -0400

    Update en.js

--- src/translations/en.js
+++ src/translations/en.js
@@ -1,16 +1,16 @@
 export default {
   components: {
-    forms: {
-      signupform: {
-        password: "Password",
-        email: "Email",
-        username: "Username",
-        confirmpassword: "Confirm Password",
-        create: "Create account",
-        facebook: "Login with Facebook",
-        termspart1: "By creating an account, you agree our ",
-        termspart2: "Terms of Service"
-      }
+    account: {
+      password: "Password",
+      email: "Email",
+      username: "Username",
+      confirmpassword: "Confirm Password",
+      create: "Create account",
+      signin: "Sign in to your account",
+      reset: "Send password reset",
+      facebook: "Login with Facebook",
+      termspart1: "By creating an account, you agree our ",
+      termspart2: "Terms of Service"
     }
   }
 };
