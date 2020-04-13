955d0d4f8b4b4edf4ad6736fb75f3d3363d83dcd
Richard <RAVMN@users.noreply.github.com>
2020-04-12 20:51:26 -0400

    Update es.js

--- src/translations/es.js
+++ src/translations/es.js
@@ -1,3 +1,16 @@
 export default {
-  test: "Hola"
+  components: {
+    forms: {
+      signupform: {
+        password: "Contraseña",
+        email: "Correo",
+        username: "Nombre de usuario",
+        confirmpassword: "Confirmar contraseña",
+        create: "Crear cuenta",
+        facebook: "Conectarse con Facebook",
+        termspart1: "Al crear la cuenta, aceptas nuestros ",
+        termspart2: "Términos de Servicio"
+      }
+    }
+  }
 };
