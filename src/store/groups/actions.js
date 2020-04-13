e8ae5ed88e58c34f66c2630a6c16633abab02103
Richard <RAVMN@users.noreply.github.com>
2020-04-13 01:46:20 -0400

    Update actions.js

--- src/store/groups/actions.js
+++ src/store/groups/actions.js
@@ -1,5 +1,6 @@
 import * as types from 'kitsu/store/types';
 import { Kitsu } from 'kitsu/config/api';
+import I18n from 'kitsu/translations/i18n';
 
 export const fetchGroupMemberships = () => async (dispatch, getState) => {
   dispatch({ type: types.FETCH_GROUP_MEMBERSHIPS });
@@ -10,6 +11,6 @@ export const fetchGroupMemberships = () => async (dispatch, getState) => {
     });
     dispatch({ type: types.FETCH_GROUP_MEMBERSHIPS_SUCCESS, payload: groups });
   } catch (e) {
-    dispatch({ type: types.FETCH_GROUP_MEMBERSHIPS_FAIL, payload: 'Failed to load groups' });
+    dispatch({ type: types.FETCH_GROUP_MEMBERSHIPS_FAIL, payload: (I18n.t("store.groups")) });
   }
 };
