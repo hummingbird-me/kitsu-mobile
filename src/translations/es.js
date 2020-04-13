24c3415059d0d6453bd10d21e4f085f774a02ba2
Richard <RAVMN@users.noreply.github.com>
2020-04-12 21:29:46 -0400

    Update es.js

--- src/translations/es.js
+++ src/translations/es.js
@@ -1,15 +1,43 @@
 export default {
   components: {
-    forms: {
-      signupform: {
-        password: "Contraseña",
-        email: "Correo",
-        username: "Nombre de usuario",
-        confirmpassword: "Confirmar contraseña",
-        create: "Crear cuenta",
-        facebook: "Conectarse con Facebook",
-        termspart1: "Al crear la cuenta, aceptas nuestros ",
-        termspart2: "Términos de Servicio"
+    account: {
+      password: "Contraseña",
+      email: "Correo",
+      username: "Nombre de usuario",
+      confirmpassword: "Confirmar contraseña",
+      create: "Crear cuenta",
+      signin: "Entrar a mi cuenta",
+      reset: "Restablecer contraseña",
+      facebook: "Conectarse con Facebook",
+      termspart1: "Al crear una cuenta, aceptas nuestros ",
+      termspart2: "Términos de Servicio"
+    },
+    button: {
+      save: "Guardar"
+    },
+    contentlist: {
+      viewall: "Ver todo"
+    },
+    datepicker: {
+      cancel: "Cancelar",
+      select: "Seleccionar una fecha",
+      confirm: "Confirmar"
+    },
+    feedback: {
+      error: "Algo falló."
+    }
+  },
+  screens: {
+    sidebar: {
+      sidebarscreen: {
+        account: "Ajustes de la cuenta",
+        settings: "Ajustes y preferencias",
+        report: "Reportar Bugs",
+        features: "Solicitar funcionalidades",
+        database: "Solicitudes de la base de datos",
+        contact: "Contáctanos",
+        profile: "Ver perfil",
+        logout: "Salir"
       }
     }
   }
