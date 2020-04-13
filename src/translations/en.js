f40d2dcdc0f310a8d2f948d3c9594aa93270d575
Richard <RAVMN@users.noreply.github.com>
2020-04-12 21:29:58 -0400

    Update en.js

--- src/translations/en.js
+++ src/translations/en.js
@@ -11,6 +11,34 @@ export default {
       facebook: "Login with Facebook",
       termspart1: "By creating an account, you agree our ",
       termspart2: "Terms of Service"
+    },
+    button: {
+      save: "Save"
+    },
+    contentlist: {
+      viewall: "View All"
+    },
+    datepicker: {
+      cancel: "Cancel",
+      select: "Select a Date",
+      confirm: "Confirm"
+    },
+    feedback: {
+      error: "Something went wrong."
+    }
+  },
+  screens: {
+    sidebar: {
+      sidebarscreen: {
+        account: "Account Settings",
+        settings: "Settings & Preferences",
+        report: "Report Bugs",
+        features: "Suggest Features",
+        database: "Database Requests",
+        contact: "Contact Us",
+        profile: "View Profile",
+        logout: "Logout"
+      }
     }
   }
 };
