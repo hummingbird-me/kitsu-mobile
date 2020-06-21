94144878745af08ba27af71161a3001d83aaf06c
Emma Lejeck <nuck@kitsu.io>
2020-06-21 11:13:40 -0700

    Allow resolving modules relative to project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
+const path = require('path')
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            app: path.resolve(__dirname, 'src')
+          },
+        },
+      ],
+    ],
   };
 };
