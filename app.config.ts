739bfc3c3f08bcf7f70c7041ccdeb7dbc42f74d8
Emma Lejeck <nuck@kitsu.io>
2020-07-19 15:19:10 -0700

    Switch from app.json to app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,24 @@
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'Kitsu',
+  slug: 'Kitsu',
+  platforms: ['ios', 'android', 'web'],
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './src/assets/icon.png',
+  splash: {
+    backgroundColor: '#402F3F',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: false,
+  },
+  extra: {
+    apiHost: 'https://kitsu.io/',
+  },
+});
