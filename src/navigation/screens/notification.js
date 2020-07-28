420b2dba421b8d5f4a0aa8b06b3a741c4cf72576
Peter Lejeck <peter.lejeck@gmail.com>
2020-07-27 19:28:07 -0700

    Strip out old navigation

--- src/navigation/screens/notification.js
+++
@@ -1,7 +0,0 @@
-import * as Screens from 'app/navigation/types';
-
-import NotificationsScreen from 'app/screens/Notifications/NotificationsScreen';
-
-export default notificationRoutes = {
-  [Screens.NOTIFICATION]: NotificationsScreen,
-};
