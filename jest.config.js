246e4a40327ddcd43ebc95b9633bc3ea5a0b3c76
Peter Lejeck <peter.lejeck@gmail.com>
2020-07-31 21:21:18 -0700

    Set up jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+// jest.config.js
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  ...tsjPreset,
+  preset: 'react-native',
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  moduleNameMapper: {
+    '^app/(.*)$': '<rootDir>/src/$1',
+  },
+  // This is the only part which you can keep
+  // from the above linked tutorial's config:
+  cacheDirectory: '.jest/cache',
+};
