1d4af6d6144a81603c59c9695f484b145943d7d5
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-02 22:14:49 -0700

    Add loginWithAssertion action

---
+++ src/actions/loginWithAssertion.ts
@@ -0,0 +1,39 @@
+import { addSeconds } from 'date-fns';
+
+import { Session } from 'app/contexts/SessionContext';
+import { LoginFailed, NetworkError } from 'app/errors';
+
+export default async function loginWithAssertion(
+  { token, provider }: { token: string; provider: 'apple' | 'facebook' },
+  init: RequestInit = {}
+): Promise<Session> {
+  const params = new URLSearchParams({
+    grant_type: 'assertion',
+    assertion: token,
+    provider,
+  });
+  const response = await fetch('https://kitsu.io/api/oauth/token', {
+    method: 'POST',
+    body: params,
+    ...init,
+  }).catch((e) => {
+    throw new NetworkError(e.message);
+  });
+  const body = await response.json();
+
+  if (response.status === 200) {
+    return {
+      accessToken: body.access_token,
+      refreshToken: body.refresh_token,
+      loggedIn: true,
+      expiresAt: addSeconds(new Date(), body.expires_in),
+    };
+  } else {
+    switch (body.error) {
+      case 'invalid_grant':
+        throw new LoginFailed(body.error_description);
+      default:
+        throw new Error(body.error_description);
+    }
+  }
+}
