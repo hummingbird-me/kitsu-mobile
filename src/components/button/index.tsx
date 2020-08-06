8575cc00c969533b3bbbc5c5f41dec53a0abc016
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-05 19:09:28 -0700

    Add ripples to button on Android

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import {
   View,
+  Platform,
   TouchableOpacity,
+  TouchableNativeFeedback,
   ActivityIndicator,
   ViewStyle,
   TextStyle,
@@ -31,24 +33,34 @@ const Button: FunctionComponent<{
 
   const { button, text } = styles.styleSheetForKind(kind);
 
-  return (
-    <TouchableOpacity
-      disabled={disabled || loading}
-      onPress={onPress}
-      style={[styles.base.button, button, style]}>
-      <View style={styles.base.contentWrapper}>
-        {loading ? (
-          <ActivityIndicator color={'rgba(255,255,255,0.6)'} />
-        ) : bare ? (
-          children
-        ) : (
-          <Text style={[styles.accessory.text, text, textStyle]}>
-            {children}
-          </Text>
-        )}
-      </View>
-    </TouchableOpacity>
+  const contents = (
+    <View style={styles.base.contentWrapper}>
+      {loading ? (
+        <ActivityIndicator color={'rgba(255,255,255,0.6)'} />
+      ) : bare ? (
+        children
+      ) : (
+        <Text style={[styles.accessory.text, text, textStyle]}>{children}</Text>
+      )}
+    </View>
   );
+
+  if (Platform.OS === 'android') {
+    return (
+      <TouchableNativeFeedback onPress={onPress} disabled={disabled || loading}>
+        <View style={[styles.base.button, button, style]}>{contents}</View>
+      </TouchableNativeFeedback>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        disabled={disabled || loading}
+        onPress={onPress}
+        style={[styles.base.button, button, style]}>
+        {contents}
+      </TouchableOpacity>
+    );
+  }
 };
 
 export default Button;
