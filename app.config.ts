9613a97d68cb289ddb3d4929aed1c8f7bcb1d6a6
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-05 19:08:27 -0700

    Reorder config params

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   platforms: ['ios', 'android', 'web'],
   version: '1.0.0',
   orientation: 'portrait',
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
   icon: './src/assets/icon.png',
   splash: {
     backgroundColor: '#402F3F',
@@ -15,10 +19,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     barStyle: 'light-content',
     backgroundColor: '#402F3F',
   },
-  updates: {
-    fallbackToCacheTimeout: 0,
-  },
-  assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: false,
   },
