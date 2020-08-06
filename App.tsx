934a2eacaf93df655fe23f62fda2578458b0e8af
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-05 19:10:07 -0700

    Load react-native-gesture-handler on boot

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { enableScreens } from 'react-native-screens';
