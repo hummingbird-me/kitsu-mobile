0d9a7abe9b6251bed9c50564e2fbd1f6a928b262
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-09 13:17:09 -0700

    Configure jest compilation for RN/Expo

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ module.exports = {
     ...tsjPreset.transform,
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
   globals: {
     'ts-jest': {
       babelConfig: true,
