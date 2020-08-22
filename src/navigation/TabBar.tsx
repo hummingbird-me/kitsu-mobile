0b71cf8438c213f0ca0db63ef6b0e50a84b76dd5
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-22 14:30:34 -0700

    Refactor TabBarNavigator

--- src/navigation/TabBar.tsx
+++ src/navigation/TabBar.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
-import {
-  Platform,
-  View,
-  TouchableWithoutFeedback,
-  TouchableWithoutFeedbackProps,
-  TouchableNativeFeedback,
-  TouchableNativeFeedbackProps,
-  GestureResponderEvent,
-  StyleSheet,
-} from 'react-native';
-import {
-  createBottomTabNavigator,
-  BottomTabBarProps,
-  BottomTabBarOptions,
-  BottomTabNavigationOptions,
-} from '@react-navigation/bottom-tabs';
-import { Route } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SvgProps } from 'react-native-svg';
 
-import {
-  extraDarkPurple,
-  tabBarPurple,
-  tabbarSelectedTextColor,
-  white,
-} from 'app/constants/colors';
 import {
   HomeIcon,
   NotificationsIcon,
@@ -30,6 +9,7 @@ import {
   SearchIcon,
 } from 'app/assets/icons/tabs';
 
+import TabBar from './components/TabBar';
 import Home from './Home';
 
 export type TabBarNavigatorParamList = {
@@ -42,83 +22,13 @@ export type TabBarNavigatorParamList = {
 
 const Tab = createBottomTabNavigator<TabBarNavigatorParamList>();
 
-type TouchableComponent = React.FC<
-  TouchableNativeFeedbackProps | TouchableWithoutFeedbackProps
->;
-
-const Touchable: TouchableComponent =
-  Platform.OS === 'ios'
-    ? (args) => <TouchableWithoutFeedback {...args} />
-    : (args) => (
-        <TouchableNativeFeedback
-          background={TouchableNativeFeedback.SelectableBackgroundBorderless()}
-          {...args}
-        />
-      );
-
-function TabBarIcon({
-  route,
-  options,
+const tabBarIconFor = (Icon: React.FC<SvgProps>) => ({
   focused,
-  onPress,
+  color,
 }: {
-  route: Route<string>;
-  options: BottomTabNavigationOptions;
   focused: boolean;
-  onPress: (e: GestureResponderEvent) => void;
-}) {
-  return (
-    <Touchable onPress={onPress} key={route.name}>
-      <View
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          flex: 1,
-        }}>
-        {options.tabBarIcon
-          ? // @ts-ignore
-            options.tabBarIcon({
-              focused,
-              color: focused ? tabbarSelectedTextColor : white,
-            })
-          : null}
-      </View>
-    </Touchable>
-  );
-}
-
-function TabBar({
-  state,
-  descriptors,
-  navigation,
-}: BottomTabBarProps<BottomTabBarOptions>) {
-  return (
-    <View
-      style={{
-        height: 50,
-        backgroundColor: tabBarPurple,
-        borderColor: extraDarkPurple,
-        borderTopWidth: StyleSheet.hairlineWidth,
-        flexDirection: 'row',
-        alignItems: 'stretch',
-      }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const focused = index === state.index;
-
-        return (
-          <TabBarIcon
-            key={route.key}
-            route={route}
-            options={options}
-            focused={focused}
-            onPress={() => navigation.navigate(route.name)}
-          />
-        );
-      })}
-    </View>
-  );
-}
+  color: string;
+}) => <Icon opacity={focused ? 1 : 0.5} color={color} />;
 
 export default function TabBarNavigator() {
   return (
@@ -126,38 +36,22 @@ export default function TabBarNavigator() {
       <Tab.Screen
         name="Home"
         component={Home}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <HomeIcon opacity={focused ? 1 : 0.5} color={color} />
-          ),
-        }}
+        options={{ tabBarIcon: tabBarIconFor(HomeIcon) }}
       />
       <Tab.Screen
         name="Search"
         component={Home}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <SearchIcon opacity={focused ? 1 : 0.5} color={color} />
-          ),
-        }}
+        options={{ tabBarIcon: tabBarIconFor(SearchIcon) }}
       />
       <Tab.Screen
         name="QuickUpdate"
         component={Home}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <QuickUpdateIcon opacity={focused ? 1 : 0.5} color={color} />
-          ),
-        }}
+        options={{ tabBarIcon: tabBarIconFor(QuickUpdateIcon) }}
       />
       <Tab.Screen
         name="Notifications"
         component={Home}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <NotificationsIcon opacity={focused ? 1 : 0.5} color={color} />
-          ),
-        }}
+        options={{ tabBarIcon: tabBarIconFor(NotificationsIcon) }}
       />
       <Tab.Screen name="Library" component={Home} />
     </Tab.Navigator>
---
+++ src/navigation/components/TabBar.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+  Platform,
+  View,
+  TouchableWithoutFeedback,
+  TouchableWithoutFeedbackProps,
+  TouchableNativeFeedback,
+  TouchableNativeFeedbackProps,
+  GestureResponderEvent,
+  StyleSheet,
+} from 'react-native';
+import {
+  BottomTabBarProps,
+  BottomTabBarOptions,
+  BottomTabNavigationOptions,
+} from '@react-navigation/bottom-tabs';
+import { Route } from '@react-navigation/native';
+
+import {
+  extraDarkPurple,
+  tabBarPurple,
+  tabbarSelectedTextColor,
+  white,
+} from 'app/constants/colors';
+
+type TouchableComponent = React.FC<
+  TouchableNativeFeedbackProps | TouchableWithoutFeedbackProps
+>;
+
+const Touchable: TouchableComponent =
+  Platform.OS === 'ios'
+    ? (args) => <TouchableWithoutFeedback {...args} />
+    : (args) => (
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.SelectableBackgroundBorderless()}
+          {...args}
+        />
+      );
+
+function TabBarIcon({
+  route,
+  options,
+  focused,
+  onPress,
+}: {
+  route: Route<string>;
+  options: BottomTabNavigationOptions;
+  focused: boolean;
+  onPress: (e: GestureResponderEvent) => void;
+}) {
+  return (
+    <Touchable onPress={onPress} key={route.name}>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        {options.tabBarIcon
+          ? // @ts-ignore
+            options.tabBarIcon({
+              focused,
+              color: focused ? tabbarSelectedTextColor : white,
+            })
+          : null}
+      </View>
+    </Touchable>
+  );
+}
+
+export default function TabBar({
+  state,
+  descriptors,
+  navigation,
+}: BottomTabBarProps<BottomTabBarOptions>) {
+  return (
+    <View
+      style={{
+        height: 50,
+        backgroundColor: tabBarPurple,
+        borderColor: extraDarkPurple,
+        borderTopWidth: StyleSheet.hairlineWidth,
+        flexDirection: 'row',
+        alignItems: 'stretch',
+      }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const focused = index === state.index;
+
+        return (
+          <TabBarIcon
+            key={route.key}
+            route={route}
+            options={options}
+            focused={focused}
+            onPress={() => navigation.navigate(route.name)}
+          />
+        );
+      })}
+    </View>
+  );
+}
