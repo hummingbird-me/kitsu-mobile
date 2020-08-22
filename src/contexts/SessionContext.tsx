71db29fea1f1f3ff4e5811737164dd6679e7fc04
Peter Lejeck <peter.lejeck@gmail.com>
2020-08-21 20:26:03 -0700

    Add clearSession function

--- src/contexts/SessionContext.tsx
+++ src/contexts/SessionContext.tsx
@@ -1,25 +1,38 @@
 import React, { useState, useContext } from 'react';
 
 import { Session } from 'app/types/session';
+import * as SessionStore from 'app/utils/session-store';
 
 export const SessionContext = React.createContext<{
   session: Session;
-  setSession: React.Dispatch<React.SetStateAction<Session>>;
+  setSession: (newSession: Session) => Promise<void>;
+  clearSession: () => Promise<void>;
 }>({
   session: null,
-  setSession: () => {},
+  setSession: async () => {},
+  clearSession: async () => {},
 });
 
 export const SessionContextProvider: React.FunctionComponent<{}> = function ({
   children,
 }) {
-  const [session, setSession] = useState<Session>(null);
+  const [session, _setSession] = useState<Session>(null);
+
+  const setSession = (newSession: Session) => {
+    _setSession(newSession);
+    return SessionStore.save(newSession);
+  };
+  const clearSession = () => {
+    _setSession(null);
+    return SessionStore.clear();
+  };
 
   return (
     <SessionContext.Provider
       value={{
         session,
         setSession,
+        clearSession,
       }}>
       {children}
     </SessionContext.Provider>
