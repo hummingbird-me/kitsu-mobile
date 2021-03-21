a6e3d524383b0820ca4459a76f2d2c023bbf6526
Emma Lejeck <nuck@kitsu.io>
2021-03-20 22:25:21 -0700

    Update to Metro 40

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,9 @@
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  } = await getDefaultConfig(__dirname);
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
