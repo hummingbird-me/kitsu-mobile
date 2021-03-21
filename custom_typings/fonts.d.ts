68aa2289758beda000e6aa991ea1bca782a6b6a9
Emma Lejeck <nuck@kitsu.io>
2021-03-21 12:03:10 -0700

    Add typings for google fonts

---
+++ custom_typings/fonts.d.ts
@@ -0,0 +1,9 @@
+declare module '@expo-google-fonts/open-sans' {
+  export { useFonts } from 'expo-font';
+  export const OpenSans_400Regular: any;
+  export const OpenSans_700Bold: any;
+}
+declare module '@expo-google-fonts/asap' {
+  export { useFonts } from 'expo-font';
+  export const Asap_700Bold: any;
+}
