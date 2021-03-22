fd7c78f19abe33cfc527e1e3ab0da5a9312d98d4
Emma Lejeck <nuck@kitsu.io>
2021-03-21 20:04:41 -0700

    Log initializer durations

--- src/initializers/index.ts
+++ src/initializers/index.ts
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import logWhen from 'app/hooks/logWhen';
 
 import loadFonts from './loadFonts';
@@ -6,30 +8,28 @@ import setupBlurhash from './setupBlurhash';
 import setupI18n from './setupI18n';
 
 export default function isBooted() {
+  const startTime = useMemo(() => Date.now(), []);
   // Check the boot requirements to see if they've finished loading
-  const BOOT_REQUIREMENTS = [
-    loadFonts,
-    loadSession,
-    setupBlurhash,
-    setupI18n,
-  ];
+  const BOOT_REQUIREMENTS = [loadFonts, loadSession, setupBlurhash, setupI18n];
   const state = BOOT_REQUIREMENTS.map((initializer) => {
     const isBooted = initializer();
     logWhen({
       current: isBooted,
       expected: true,
       level: 'debug',
-      message: `Initializers: ✅ ${initializer.name}`
+      message: `Initializers: ✅ ${initializer.name} (${
+        Date.now() - startTime
+      }ms)`,
     });
     return isBooted;
-  })
+  });
   const booted = state.reduce((acc, val) => acc && val, true);
 
   logWhen({
     current: booted,
     expected: true,
     level: 'debug',
-    message: 'Initializers: 🎉  Done!'
+    message: `Initializers: 🎉  Done in ${Date.now() - startTime}ms!`,
   });
 
   return booted;
