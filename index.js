74c9604420d5678e51f6c50d30d14b5abd6cad15
Emma Lejeck <nuck@kitsu.io>
2022-12-28 20:12:01 -0800

    Configure Facebook SDK for Android

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import * as Sentry from '@sentry/react-native';
 import { LogBox } from 'react-native';
+import Config from 'react-native-config';
+import { Settings as Facebook } from 'react-native-fbsdk-next';
 import { Navigation } from 'react-native-navigation';
 import OneSignal from 'react-native-onesignal';
 
 import { Layouts, defaultOptions, registerScreens } from 'kitsu/navigation';
 
 OneSignal.setAppId('01f6e47a-6809-4118-a796-949952e9c209');
+Facebook.setAppID(Config.FACEBOOK_APP_ID);
+Facebook.initializeSDK();
+
 LogBox.ignoreLogs(['Could not find Fiber with id']);
 
 window.btoa = require('base-64').encode;
