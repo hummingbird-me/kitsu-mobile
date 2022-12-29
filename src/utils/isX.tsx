ab009c3602d00654543afe048e0f0929ecb7fd23
Emma Lejeck <nuck@kitsu.io>
2022-12-28 20:36:54 -0800

    Wire isX util to rn-safe-area-context

--- src/utils/isX.tsx
+++ src/utils/isX.tsx
@@ -1,27 +1,18 @@
-import { Dimensions, Platform } from 'react-native';
-
-// See https://mydevice.io/devices/ for device dimensions
-const X_WIDTH = 375;
-const X_HEIGHT = 812;
-const XSMAX_WIDTH = 414;
-const XSMAX_HEIGHT = 896;
-
-const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window');
+import { Platform } from 'react-native';
+import { initialWindowMetrics } from 'react-native-safe-area-context';
 
 // The top inset is 44 (24 + status bar height)
 // However we subtract the status bar height in this
-export const safeAreaInsetX = { top: 24, bottom: 34 };
+export const safeAreaInsetX = {
+  top: initialWindowMetrics ? initialWindowMetrics.insets.top - 24 : 0,
+  bottom: initialWindowMetrics ? initialWindowMetrics.insets.bottom : 0,
+};
 export const paddingX = safeAreaInsetX.top;
 
 export const isX = (() => {
-  if (Platform.OS === 'web') return false;
+  if (Platform.OS !== 'ios') return false;
+  if (!initialWindowMetrics) return false;
 
-  const X =
-    (D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
-    (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT);
-  const XSMAX =
-    (D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) ||
-    (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT);
-
-  return Platform.OS === 'ios' && (X || XSMAX);
+  if (initialWindowMetrics.insets.top > 0) return true;
+  if (initialWindowMetrics.insets.bottom > 0) return true;
 })();
