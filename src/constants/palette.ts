f53eb682d42dd2f38e672a2350884bc05d1bb518
Emma Lejeck <nuck@kitsu.io>
2023-01-01 15:06:14 -0800

    Add new palette

---
+++ src/constants/palette.ts
@@ -0,0 +1,82 @@
+export const white = '#ffffff';
+export const black = '#000000';
+
+export const red = {
+  1: '#f6c8c5',
+  2: '#eca39e',
+  3: '#e27a70',
+  4: '#d5524c',
+  5: '#b33f39',
+  6: '#932828',
+  7: '#751923',
+};
+
+export const pink = {
+  1: '#f6c7d4',
+  2: '#eba1b8',
+  3: '#df779b',
+  4: '#ce5186',
+  5: '#ae3d6e',
+  6: '#8a2b58',
+  7: '#6e1c47',
+};
+
+export const yellow = {
+  1: '#f8dabb',
+  2: '#f5bc72',
+  3: '#eaa546',
+  4: '#cd8c2f',
+  5: '#ae792a',
+  6: '#8a5f21',
+  7: '#7c5314',
+};
+
+export const green = {
+  1: '#aeecd9',
+  2: '#7ad4bc',
+  3: '#4cbba0',
+  4: '#16a085',
+  5: '#068d75',
+  6: '#00745f',
+  7: '#005c4b',
+};
+
+export const blue = {
+  1: '#c4ddf6',
+  2: '#99c6f2',
+  3: '#5aaeee',
+  4: '#3d95d1',
+  5: '#3085bf',
+  6: '#2671a7',
+  7: '#185e95',
+};
+
+export const purple = {
+  1: '#dccbe8',
+  2: '#c7a9dc',
+  3: '#b381d3',
+  4: '#9667ba',
+  5: '#7f50a0',
+  6: '#583674',
+  7: '#462862',
+};
+
+export const kitsuPurple = {
+  1: '#c9b4c7',
+  2: '#968094',
+  3: '#6f5a6d',
+  4: '#503d4f',
+  5: '#443443',
+  6: '#3b2d3b',
+  7: '#2d1c2b',
+};
+
+export const grey = {
+  1: '#f8f8f8',
+  2: '#edf0f1',
+  3: '#d2d5d9',
+  4: '#afb5bc',
+  5: '#8b8f95',
+  6: '#6a6c70',
+  7: '#434445',
+};
