e433327a0c0eb23b4b5c547d04e8b9e983064ad4
Emma Lejeck <nuck@kitsu.io>
2024-08-15 18:02:58 -0700

    Add root gesture handler

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import UrqlContextProvider from './UrqlContext';
 
 const ApplicationContext: React.FC<{ children: React.ReactNode }> = function ({
   children,
 }) {
-  return <UrqlContextProvider>{children}</UrqlContextProvider>;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <UrqlContextProvider>{children}</UrqlContextProvider>
+    </GestureHandlerRootView>
+  );
 };
 export default ApplicationContext;
