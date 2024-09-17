949f6a1a19bb40741675549c14e4154164c44a0a
Emma Lejeck <nuck@kitsu.io>
2024-09-17 01:40:26 -0700

    Delete old navigation code

--- src/navigation/screens/notification.tsx
+++
@@ -1,6 +0,0 @@
-import * as Screens from 'kitsu/navigation/types';
-import NotificationsScreen from 'kitsu/screens/Notifications/NotificationsScreen';
-
-export default notificationRoutes = {
-  [Screens.NOTIFICATION]: NotificationsScreen,
-};
