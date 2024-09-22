48bd1cdc108bbe118bc1aa903df5a96d67565193
Emma Lejeck <nuck@kitsu.io>
2024-09-22 14:26:47 -0700

    Add deprecations to old colors

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,46 +1,112 @@
 /* reds/pinks */
+/**
+ * @deprecated Use palette.red[4] instead
+ */
 export const red = '#DA5E51';
+/**
+ * @deprecated Use palette.red[3] instead
+ */
 export const activeRed = '#F87270';
+/**
+ * @deprecated Use palette.pink[1] instead
+ */
 export const lightPink = '#B7A7B6';
 export const tabRed = '#fc755c';
 export const tabInactive = '#A199A1';
 
 /* blues */
+/**
+ * @deprecated Use palette.blue[3] instead
+ */
 export const blue = '#43ABF1';
 export const fbBlue = '#507CC0';
 export const fbBlueDark = '#3B5998'; // taken from invision app designs
 export const linkBlue = '#083d8d'; // sidebar seemore text
 
 /* purples */
-export const extraDarkPurple = '#302530';
+/**
+ * @deprecated Use palette.kitsuPurple[7] instead
+ */
+export const extraDarkPurple = '#090809';
+/**
+ * @deprecated Use palette.kitsuPurple[6] instead
+ */
 export const darkPurple = '#402F3F';
+/**
+ * @deprecated Use palette.kitsuPurple[3] instead
+ */
 export const lightPurple = '#624E5F';
+/**
+ * @deprecated Use palette.kitsuPurple[5] instead
+ */
 export const listBackPurple = '#443443';
+/**
+ * @deprecated Use palette.kitsuPurple[4] instead
+ */
 export const tabBarPuple = '#453342';
 
 /* greens */
+/**
+ * @deprecated Use palette.green[4] instead
+ */
 export const green = '#16A085';
+/**
+ * @deprecated Use palette.green[2] instead
+ */
 export const lightGreen = '#84E06F';
 
 /* whites/greys */
+/**
+ * @deprecated Use palette.white instead
+ */
 export const white = '#FFFFFF';
+/**
+ * @deprecated Use palette.grey[1] instead
+ */
 export const offWhite = '#FAFAFA';
 export const transparentWhite = 'rgba(255, 255, 255, 0.6)'; // signup tabs
+/**
+ * @deprecated Use palette.grey[3] instead
+ */
 export const imageGrey = '#D8D8D8';
+/**
+ * @deprecated Use palette.grey[3] instead
+ */
 export const lightGrey = '#CCCCCC';
+/**
+ * @deprecated Use palette.grey[2] instead
+ */
 export const lightestGrey = '#EEEEEE';
+/**
+ * @deprecated Use palette.grey[6] instead
+ */
 export const darkGrey = '#696969';
-export const grey = '#808080'; // html grey aka 'grey'
+/**
+ * @deprecated Use palette.grey[5] instead
+ */
+export const grey = '#808080';
 
 /* yellows */
-export const yellow = '#F0AD4E';
+/**
+ * @deprecated Use palette.yellow[2] instead
+ */
+export const yellow = '#F0AD4E'; // yellow[2]
 
 /* orange */
 export const orange = '#D95E40';
 
 /* blacks */
+/**
+ * @deprecated Use palette.black instead
+ */
 export const black = '#000000';
+/**
+ * @deprecated Use palette.grey[7] instead
+ */
 export const offBlack = '#2B212B';
+/**
+ * @deprecated Use palette.grey[7] instead
+ */
 export const softBlack = '#333';
 
 /* misc */
