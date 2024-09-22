d7a948cc2612a6dd3246356160c9c40b16b32af5
Emma Lejeck <nuck@kitsu.io>
2024-09-21 21:05:00 -0700

    Refactor screens/Feed/components/TabBar

--- src/screens/Feed/components/TabBar/component.tsx
+++ src/screens/Feed/components/TabBar/component.tsx
@@ -1,58 +1,45 @@
 import React from 'react';
 import { TouchableOpacity, View } from 'react-native';
 
-import { StyledText } from 'kitsu/components/StyledText';
-import { defaultAvatar } from 'kitsu/constants/app';
-import { Avatar } from 'kitsu/screens/Feed/components/Avatar';
+import { StyledText } from '@/components/StyledText';
+import AvatarImage from '@/components/content/AvatarImage';
+import { useAccount } from '@/contexts/AccountContext';
+import { useDrawer } from '@/contexts/DrawerContext';
 
 import { styles } from './styles';
 
-interface TabBarProps {
-  currentUser: object;
-  onPress(...args: unknown[]): unknown;
+type TabBarProps = {
   children?: React.ReactNode;
-}
+};
 
-export const TabBar = ({ currentUser, onPress, children }: TabBarProps) => (
-  <View style={styles.tabBar}>
-    <TouchableOpacity style={styles.tabAvatar} onPress={onPress}>
-      <Avatar
-        size="small"
-        avatar={
-          (currentUser && currentUser.avatar && currentUser.avatar.medium) ||
-          defaultAvatar
-        }
-      />
-    </TouchableOpacity>
-    {children}
-  </View>
-);
+export const TabBar = ({ children }: TabBarProps) => {
+  const { profile } = useAccount();
+  const drawer = useDrawer();
 
-interface TabBarLinkProps {
+  return (
+    <View style={styles.tabBar}>
+      <TouchableOpacity
+        style={styles.tabAvatar}
+        onPress={() => drawer?.current.openDrawer()}>
+        <AvatarImage size={24} source={profile.avatarImage} />
+      </TouchableOpacity>
+      {children}
+    </View>
+  );
+};
+
+type TabBarLinkProps = {
   onPress?(...args: unknown[]): unknown;
   label?: string;
   isActive?: boolean;
-}
+};
 
 export const TabBarLink = ({ onPress, label, isActive }: TabBarLinkProps) => (
   <TouchableOpacity
     onPress={onPress}
-    style={[styles.tabStyle, isActive && styles.tabStyle__active]}
-  >
+    style={[styles.tabStyle, isActive && styles.tabStyle__active]}>
     <StyledText color={isActive ? 'light' : 'grey'} size="xsmall" bold>
       {label}
     </StyledText>
   </TouchableOpacity>
 );
-
-TabBar.defaultProps = {
-  currentUser: null,
-  onPress: null,
-  children: null,
-};
-
-TabBarLink.defaultProps = {
-  onPress: null,
-  label: '',
-  isActive: false,
-};
